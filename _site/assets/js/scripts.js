b01e4fcb2020ad159d330651f75672ffeebe8b8f
ΓΣ <64132830+g-stavridis@users.noreply.github.com>
2023-09-10 13:55:51 +0300

    Update scripts.js

---
+++ _site/assets/js/scripts.js
@@ -0,0 +1,11 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    fullscreen: true
+});
+
+const players = Plyr.setup('.js-player');
