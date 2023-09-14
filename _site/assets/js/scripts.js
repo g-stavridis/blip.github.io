94a89c2e041ff12c326856923e4847a3d291445c
ΓΣ <64132830+g-stavridis@users.noreply.github.com>
2023-09-15 00:17:53 +0300

    flickity resize

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -8,4 +8,18 @@ var flkty = new Flickity(elem, {
     wrapAround: true,
     fullscreen: true
 });
+
+Flickity.prototype._createResizeClass = function () {
+    this.element.classList.add('flickity-resize');
+};
+
+Flickity.createMethods.push('_createResizeClass');
+
+var resize = Flickity.prototype.resize;
+Flickity.prototype.resize = function () {
+    this.element.classList.remove('flickity-resize');
+    resize.call(this);
+    this.element.classList.add('flickity-resize');
+};
+
 const players = Plyr.setup('.js-player');
